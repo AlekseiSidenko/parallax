19a628ab7932a2953f47bcaaca4b50206fe54c1a
Aleksei Sidenko <sidoi90@bk.ru>
2024-12-23 21:57:19 +0500

    change animation to frame-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import { useEffect } from 'react'
+import { useRef } from 'react'
 import './App.css'
+import {
+  motion,
+  useScroll,
+  useTransform,
+  MotionValue
+} from "framer-motion";
 
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [0, distance]);
+}
 
-function App(){
+function App() {
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      document.documentElement.style.setProperty('--scrollTop', `${window.scrollY}px`)
-    })
-  }, [])
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const layerHeaderY = useParallax(scrollYProgress, 430);
+  const baseLayerY = useParallax(scrollYProgress, 540);
+  const middleLayerY = useParallax(scrollYProgress, 350);
+  const frontLayerY = useParallax(scrollYProgress, 150);
+  const mainArticleY = useParallax(scrollYProgress, 115);
 
   return (
-    <div className="wrapper">
-      <div className="content">
+    <div className="wrapper" ref={ref}>
+        <div className="content">
 
-        <header className="main-header">
-          <div className="layers">
-            <div className="layer__header">
-              <div className="layers__caption">Welcome to Parallax</div>
-              <div className="layers__title">Fairy Forest</div>
+          <header className="main-header">
+            <div className="layers">
+              <motion.div className="layer__header" style={{ y: layerHeaderY }}>
+                <div className="layers__caption">Здарова заебал</div>
+                <div className="layers__title">Пералакс</div>
+              </motion.div>
+              <motion.div className="layer layers__base" style={{ y: baseLayerY }}></motion.div>
+              <motion.div className="layer layers__middle" style={{ y: middleLayerY }}></motion.div>
+              <motion.div className="layer layers__front" style={{ y: frontLayerY }}></motion.div>
             </div>
-            <div className="layer layers__base"></div>
-            <div className="layer layers__middle"></div>
-            <div className="layer layers__front"></div>
-          </div>
-        </header>
+          </header>
 
-        <article className="main-article">
-          <div className="main-article__content">
-            <h2 className="main-article__header">To be continued</h2>
-            <p className="main-article__paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error provident dignissimos facere. Repellendus tempore autem qui! Quia magnam tempora esse id necessitatibus corrupti mollitia expedita sapiente cum rerum, ut dicta laboriosam!</p>
-          </div>
-        </article>
-      </div>
+          <motion.article className="main-article" style={{ y: mainArticleY }}>
+            <div className="main-article__content">
+              <h2 className="main-article__header">ОЛЕГ!!!</h2>
+              <p className="main-article__paragraph">Давай норм картинку подберем и тут дальше длинный такой текст как ты любишь: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error provident dignissimos facere. Repellendus tempore autem qui! Quia magnam tempora esse id necessitatibus corrupti mollitia expedita sapiente cum rerum, ut dicta laboriosam!</p>
+              <p className="main-article__paragraph">А и еще скролл доработаю и удалю этот текст</p>
+            </div>
+          </motion.article>
+        </div>
     </div>
   )
 }
